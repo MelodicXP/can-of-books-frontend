6eb788e1633fd2522b8aca28dd1a60b79d21ae4e
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-11-06 17:13:22 -0600

    Add link to about section

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import './App.css';
 
 class Header extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* PLACEHOLDER: render a navigation link to the about page */}
+        <Navbar.Brand className='nav-bar-brand'>My Favorite Books</Navbar.Brand>
+        <NavItem className='nav-links'><Link to="/" className="nav-link">Home</Link></NavItem>
+        <NavItem className='nav-links'><Link to="/about" className="nav-link">About</Link></NavItem>
       </Navbar>
     )
   }
