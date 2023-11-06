fa5315824f56d1ed71da7f2263bfb6101536e456
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-11-06 17:09:33 -0600

    Retrieve book data and render in carousel format

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './App.css';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,6 +12,10 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    axios.get('http://localhost:3001/books')
+      .then(res => this.setState( {books: res.data} ) )
+  }
 
   render() {
 
@@ -18,8 +25,19 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+          <Carousel data-bs-theme="dark">
+            {this.state.books.map((book) => (
+              <Carousel.Item key={book._id} className='carousel-item'>
+                <img src={book.imageUrl}  alt={book.title} />
+                <Carousel.Caption className='carousel-caption'>
+                  <h3>{book.title}</h3>
+                  <p>{book.description} <br /></p>
+                  <p>{`Status: ${book.status}`}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
