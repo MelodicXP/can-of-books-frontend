4b6d53d1dc57546068b7a088ab82d7a3083bff1f
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-11-25 16:59:03 -0600

    Add message if user not logged in

---
+++ src/Welcome.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Welcome extends Component {
+
+  render() {
+    /* TODO: render information about the developers */
+    return <p id='login-message'>Please Log In</p>
+  }
+}
+
+export default Welcome;
